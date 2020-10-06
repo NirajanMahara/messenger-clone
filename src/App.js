92ccdec24f71628d6c22b41506b7c7dde3ce69f1
ufuk <ufuk.uysl5@gmail.com>
2020-10-06 14:13:09 +0200

    create basic react controlled form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 function App() {
+  const [state, setstate] = useState("");
+  const [messages, setMessages] = useState(["hey"]);
+
+  console.log(state);
+  console.log(messages);
+
+  const inputHandler = (e) => {
+    setstate(e.target.value);
+  };
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    //copy all the existing messages and new one.
+    setMessages([...messages, state]);
+
+    //reset input field after clicking button
+    setstate("");
+  };
   return (
     <>
-      <h1>Hey</h1>
-      <input></input>
-      <button>Send Message</button>
+      <form>
+        <input value={state} onChange={inputHandler}></input>
+        <button type="submit" onClick={sendMessage}>
+          Send Message
+        </button>
+      </form>
+
+      {messages.map((item) => (
+        <p>{item}</p>
+      ))}
     </>
   );
 }
