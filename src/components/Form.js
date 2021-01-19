12ebc1917f807a83aea05478015273bb9c6d8230
u-uysal <ufuk.uysl5@gmail.com>
2021-01-19 23:43:39 +0100

    fix bugs

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,36 +2,32 @@ import React from "react";
 
 function Form({ state, sendMessage, inputHandler, onKeyPress }) {
   return (
-    <div className="row">
-      <div className="col-12 fixed-bottom">
-        <form className="form-send-message center-items">
-          <div className="row">
-            <div className="col-9 form-input">
-              <input
-                type="text"
-                id="message"
-                name="message"
-                value={state}
-                onKeyPress={onKeyPress}
-                placeholder="Please enter your message"
-                onChange={inputHandler}
-                className="form-control "
-              ></input>
-            </div>
-            <div className="col-1 form-button">
-              <button
-                type="button"
-                onClick={sendMessage}
-                disabled={!state}
-                className="btn btn-info"
-              >
-                Send
-              </button>
-            </div>
-          </div>
-        </form>
+    <form className="form-send-message center-items fixed-bottom w-50">
+      <div className="row mt-2">
+        <div className="col-9 form-input">
+          <input
+            type="text"
+            id="message"
+            name="message"
+            value={state}
+            onKeyPress={onKeyPress}
+            placeholder="Please enter your message"
+            onChange={inputHandler}
+            className="form-control "
+          ></input>
+        </div>
+        <div className="col-1 form-button">
+          <button
+            type="button"
+            onClick={sendMessage}
+            disabled={!state}
+            className="btn btn-info"
+          >
+            Send
+          </button>
+        </div>
       </div>
-    </div>
+    </form>
   );
 }
 
